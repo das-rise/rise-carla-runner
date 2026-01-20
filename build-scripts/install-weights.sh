2b411541ed2a34c7867decc9d363df0f9deac9ac
Bastian Havers-Zulka <bastian.havers-zulka@ri.se>
2026-01-20 20:56:49 +0100

    update path of zipped pretrained weights

--- build-scripts/install-weights.sh
+++ build-scripts/install-weights.sh
@@ -4,4 +4,4 @@ if [ ! -f pretrained.zip ]; then
     echo "Downloading pretrained weights, this may take a while..."
     wget https://zenodo.org/records/17110968/files/pretrained.zip?download=1 -O "$ZIPNAME"
 fi
-unzip "$ZIPNAME" -d ../src/rirun/PCLA/agents/.
+unzip "$ZIPNAME" -d src/rirun/PCLA/agents/.

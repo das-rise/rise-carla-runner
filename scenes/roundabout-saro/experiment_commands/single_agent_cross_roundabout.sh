3238f39b445b905017f6feeec95a22dcea7bc39c
Ramana Avula <r2avula@gmail.com>
2026-01-29 10:30:52 +0100

    Fix file paths in roundabout and traffic scripts for consistency to launch from repository root

--- scenes/roundabout-saro/experiment_commands/single_agent_cross_roundabout.sh
+++ scenes/roundabout-saro/experiment_commands/single_agent_cross_roundabout.sh
@@ -1,10 +1,10 @@
 export DIRECT=0
-python run.py \
+python src/rirun/run.py \
   45 \
-  ../../scenes/roundabout-saro/maps/saro_fixed_bidirectional_WIDER_LANES.xodr \
-  ../../scenes/roundabout-saro/npc_test_routes/at_big_roundabout.csv \
+  scenes/roundabout-saro/maps/saro_fixed_bidirectional_WIDER_LANES.xodr \
+  scenes/roundabout-saro/npc_test_routes/at_big_roundabout.csv \
   --movement teleport \
   --timestep 0.0333333333333333333 \
   --pcla_agent tfpp_wp_1 \
-  --pcla_route ../../scenes/roundabout-saro/agent_routes/across_big_roundabout.xml \
+  --pcla_route scenes/roundabout-saro/agent_routes/across_big_roundabout.xml \
   --ego_camera
--- scenes/town01/experiment_commands/turn_into_traffic_simlingo_prompted.sh
+++ scenes/town01/experiment_commands/turn_into_traffic_simlingo_prompted.sh
@@ -3,11 +3,11 @@ export SIMLINGO_USER_FLAG=1
 python run.py \
     15 \
     Town01 \
-    ../../scenes/town01/npc_test_routes/vehicle1_straight.csv \
+    scenes/town01/npc_test_routes/vehicle1_straight.csv \
     --movement teleport \
     --timestep 0.0333333333333333333 \
     --pcla_agent simlingo_rirun \
-    --pcla_route ../../scenes/town01/agent_routes/agent_turn.xml \
+    --pcla_route scenes/town01/agent_routes/agent_turn.xml \
     --ego_camera
 
 unset SIMLINGO_CUSTOM_PROMPT

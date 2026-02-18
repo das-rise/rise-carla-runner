f6bc9472a0e25424c9051741ca0df0e2b673cc38
Bastian Havers-Zulka <bastian.havers-zulka@ri.se>
2026-02-18 11:13:11 +0100

    set initial camera even to non-initialized vehicle

--- src/rirun/run.py
+++ src/rirun/run.py
@@ -175,7 +175,7 @@ def main() -> None:
         settings.fixed_delta_seconds = timestep
     world.apply_settings(settings)
 
-    logging.info(f"{world.get_settings()}")
+    logging.info(f"Carla world settings: \n{world.get_settings()}")
 
     traj_recorder = Carla2Traj(world, debug=False)
 
@@ -231,6 +231,8 @@ def main() -> None:
         )
         vehicles.append(pcla_vehicle)
 
+    logging.info(f"Vehicle list: {','.join([v.name for v in vehicles])}")
+
     success = True
     start_ts = datetime.now().strftime("%Y%m%d_%H%M%S")
     try:
@@ -248,7 +250,8 @@ def main() -> None:
         ## CAMERA
 
         # Get start location of first trajectory and set camera there
-        start_of_trajectory = [v for v in vehicles if hasattr(v, "_actor")][
+
+        start_of_trajectory = [v for v in vehicles][
             0
         ].first_trajectory_point.transform.location
         cam_loc = (start_of_trajectory.x, start_of_trajectory.y, 20)

d355e3b3ad7926e85546299134a4c0591cac1580
Bastian Havers-Zulka <bastian.havers-zulka@ri.se>
2026-02-25 07:33:59 +0100

    update real_data.sh to use shifted map, add shifted map, add example trajectories

--- scenes/roundabout-saro/experiment_commands/real_data.sh
+++ scenes/roundabout-saro/experiment_commands/real_data.sh
@@ -1,10 +1,10 @@
 export DIRECT=0
 python src/rirun/run.py \
   40 \
-  scenes/roundabout-saro/maps/saro_fixed_bidirectional_WIDER_LANES.xodr \
-  saro_roundabout_converted/vehicle_*Car.csv \
+  scenes/roundabout-saro/maps/SaroRound_2lane2_shifted_wider.xodr \
+  scenes/roundabout-saro/npc_test_routes/vehicle_*Car.csv \
   --movement teleport \
   --timestep 0.0333333333333333333 \
-  --offset 1013 \
+  --offset_time 1013 \
   --camera_mode overhead \
-  --overhead_camera_position 1700 -700 75
+  --overhead_camera_position 100 -49 150

ed641b58aa50dedbca570318e2ed32d3a2127f9c
Bastian Havers-Zulka <bastian.havers-zulka@ri.se>
2026-04-01 15:04:53 +0200

    restructure scenes

---
+++ scenes/roundabout-saro/scene_1_single_agent_into_roundabout/scene_1_3_single_agent_into_roundabout.sh
@@ -0,0 +1,12 @@
+export DIRECT=0
+python src/rirun/run.py \
+  45 \
+  scenes/roundabout-saro/scene_1_single_agent_into_roundabout/SaroRound_2lane_u_adj_14.xodr \
+  --trajectory-filepaths saro_roundabout_converted3/*Car.csv \
+  --movement teleport \
+  --timestep 0.0333333333333333333 \
+  --offset_time 1013 \
+  --pcla_agent tfpp_wp_1 \
+  --pcla_route scenes/roundabout-saro/scene_1_single_agent_into_roundabout/scene_1_3_single_agent_into_roundabout.xml \
+  --camera_mode overhead \
+  --overhead_camera_position 0 0 150

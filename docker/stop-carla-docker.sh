4546b42f875764250837736e09ddc1790d4d03a3
Bastian Havers-Zulka <bastian.havers-zulka@ri.se>
2026-04-07 13:20:26 +0200

    add --force option to kill found container directly

--- docker/stop-carla-docker.sh
+++ docker/stop-carla-docker.sh
@@ -1,12 +1,22 @@
+#!/usr/bin/env bash
+force=false
+if [ "$1" = "--force" ]; then
+  force=true
+fi
+
 containerid=$(docker ps | grep carla | awk ' { print $1 } ')
 if [ -z "$containerid" ]; then
   echo "No running Carla container found."
 else
-  trap 'echo; echo "Kill aborted."; exit 130' INT
-  printf "Found container: %s. Kill? [enter or Ctrl+C] " "$containerid"
-  read -s -r  # -s = silent, input is not echoed
-  echo    # add newline
-  docker kill "$containerid" > /dev/null 2>&1
-  echo "Container $containerid killed."
+  if [ "$force" = true ]; then
+    docker kill "$containerid" > /dev/null 2>&1
+    echo "Container $containerid killed."
+  else
+    trap 'echo; echo "Kill aborted."; exit 130' INT
+    printf "Found container: %s. Kill? [enter or Ctrl+C] " "$containerid"
+    read -s -r
+    echo
+    docker kill "$containerid" > /dev/null 2>&1
+    echo "Container $containerid killed."
+  fi
 fi
-

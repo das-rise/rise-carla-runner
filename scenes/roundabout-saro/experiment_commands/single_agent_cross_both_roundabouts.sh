f44b80ad08eec86cd76a034e7cf490b25e135946
Bastian Havers-Zulka <bastian.havers-zulka@ri.se>
2026-05-27 10:27:02 +0200

    remove local experiments, keep only examples

--- scenes/roundabout-saro/experiment_commands/single_agent_cross_both_roundabouts.sh
+++
@@ -1,8 +0,0 @@
-python src/rirun/run.py \
-    60 \
-    scenes/roundabout-saro/maps/saro_fixed_bidirectional_WIDER_LANES.xodr \
-    --npc_movement teleport \
-    --timestep 0.0333333333333333333 \
-    --ego_agent neat_aim2dsem \
-    --ego_route_filepath scenes/roundabout-saro/agent_routes/double_roundabout.xml \
-    --record_cameras ego_overhead

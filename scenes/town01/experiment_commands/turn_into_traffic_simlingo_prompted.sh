a6d7e253731b64a0a549436772e2dfa463726c7d
Bastian Havers-Zulka <bastian.havers-zulka@ri.se>
2026-05-27 10:10:31 +0200

    add more example flags

--- scenes/town01/experiment_commands/turn_into_traffic_simlingo_prompted.sh
+++ scenes/town01/experiment_commands/turn_into_traffic_simlingo_prompted.sh
@@ -8,8 +8,7 @@ carla_docker_restart
 python src/rirun/run.py \
     15 \
     Town01 \
-    --npc_trajectory_filepaths scenes/roundabout-saro/npc_test_routes/at_big_roundabout.csv \
-    scenes/town01/npc_test_routes/vehicle1_straight.csv \
+    --npc_trajectory_filepaths scenes/town01/npc_test_routes/vehicle1_straight.csv \
     --npc_movement pid \
     --timestep 0.0333333333333333333 \
     --ego_agent simlingo_rirun \
@@ -18,3 +17,10 @@ python src/rirun/run.py \
 
 unset SIMLINGO_CUSTOM_PROMPT
 unset SIMLINGO_USER_FLAG
+
+# Examples of additional run.py CLI flags that are currently unused here:
+#   --output_dir output/turn_into_traffic
+#   --display_camera stationary_overhead
+#   --mapmatch
+#   --offset_time 2.0
+#   --seed 42
